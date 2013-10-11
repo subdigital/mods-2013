59c13601a776d4770981324b03787a428ee0cf1e
Ben Scheirman <ben@scheirman.com>
2013-10-11 09:43:27 +0530

    add missing test

--- UnitTestDemo/UnitTestDemoTests/GuessTheNumberGameTests.m
+++ UnitTestDemo/UnitTestDemoTests/GuessTheNumberGameTests.m
@@ -19,10 +19,40 @@
 
 - (void)setUp {
     [super setUp];
+    
+    self.game = [[GuessTheNumberGame alloc] initWithMax:10];
 }
 
 - (void)tearDown {
     [super tearDown];
+}
+
+- (void)testCanCreateGame {
+    XCTAssertNotNil(self.game, @"game should not be nil");
+}
+
+- (void)testGameKeepsTrackOfGuesses {
+    [self.game guess:4];
+    [self.game guess:5];
+    
+    XCTAssertEqual(self.game.numberOfGuesses, 2, @"Game should have counted 2 guesses");
+}
+
+- (void)testGuessSignifiesWhenItIsIncorrect {
+    XCTAssertEqual([self.game guess:-1], GuessIncorrect, @"Guess should have returned incorrect guess");
+}
+
+- (void)testGameHasAMinimumNumber {
+    XCTAssertEqual(self.game.minimumNumber, 1, @"Min number should have been 1");
+}
+
+- (void)testGameHasADefaultMaximumOf10 {
+    XCTAssertEqual(self.game.maximumNumber, 10, @"Max should default to 10");
+}
+
+- (void)testGameCanBeInitializedWithAMaxNumber {
+    self.game = [[GuessTheNumberGame alloc] initWithMax:12];
+    XCTAssertEqual(self.game.maximumNumber, 12, @"Max should have been 12");
 }
 
 @end
